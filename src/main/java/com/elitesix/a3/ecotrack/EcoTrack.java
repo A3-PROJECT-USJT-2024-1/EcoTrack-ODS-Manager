e7573e9ff5a4386037549bb01b662ef7ae4dae18
12101863 <12101863@HSMTIC007.hsm.int>
2024-05-22 11:41:51 -0300

    Primeiro Commit

---
+++ src/main/java/com/elitesix/a3/ecotrack/EcoTrack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.elitesix.a3.ecotrack;
+
+/**
+ *
+ * @author 12101863
+ */
+public class EcoTrack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
