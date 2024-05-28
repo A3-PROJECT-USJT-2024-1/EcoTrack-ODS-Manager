d11ec7a098f59c70a9c9576b9b87dce52d503331
FelipeDGrandel <felipe.veigarpm@gmai.com>
2024-05-28 20:53:20 -0300

    Telas Felipe

---
+++ src/main/java/Telas/TelaProjetoGerenciarUsu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Model;
+
+/**
+ *
+ * @author eu
+ */
+public class TelaProjetoGerenciarUsu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
