607b3cea67d9f82ddecdad858570651e129b54d5
Soarresz <164957235+Soarresz@users.noreply.github.com>
2024-06-02 20:32:23 -0300

    viniRetosito

---
+++ src/main/java/Telas/TelaMenuProjeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gerenciador.telamenuprojeto;
+
+/**
+ *
+ * @author vini
+ */
+public class TelaMenuProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
