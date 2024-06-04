e5575c8b6b73b29359194e46b520ecfed8a4122d
Rafaella <Rafaella@Rafashe>
2024-06-03 22:39:46 -0300

    mudança de telas

--- src/main/java/Telas/TelaDetalheProjeto.java
+++ src/main/java/Telas/TelaDetalheProjeto.java
@@ -29,13 +29,11 @@ public class TelaDetalheProjeto extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         lbl_Tema = new javax.swing.JLabel();
         lbl_Data = new javax.swing.JLabel();
-        txt_ID = new javax.swing.JTextField();
         lbl_NomeDoProjeto = new javax.swing.JLabel();
         lbl_Criador = new javax.swing.JLabel();
         btn_Concluido = new javax.swing.JButton();
         btn_Curtir = new javax.swing.JButton();
         lbl_Descricao = new javax.swing.JLabel();
-        btn_Procurar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -53,19 +51,15 @@ public class TelaDetalheProjeto extends javax.swing.JFrame {
         lbl_Data.setForeground(new java.awt.Color(255, 255, 255));
         lbl_Data.setText("\"DD/MM/AA\"");
 
-        txt_ID.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        txt_ID.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        txt_ID.setText("ID");
-
         lbl_NomeDoProjeto.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         lbl_NomeDoProjeto.setForeground(new java.awt.Color(255, 255, 255));
         lbl_NomeDoProjeto.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lbl_NomeDoProjeto.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Nome do Projeto", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Liberation Sans", 0, 15), new java.awt.Color(255, 255, 255))); // NOI18N
+        lbl_NomeDoProjeto.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Nome do Projeto", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
 
         lbl_Criador.setBackground(new java.awt.Color(51, 51, 51));
         lbl_Criador.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         lbl_Criador.setForeground(new java.awt.Color(255, 255, 255));
-        lbl_Criador.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Criador:", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Liberation Sans", 0, 15), new java.awt.Color(255, 255, 255))); // NOI18N
+        lbl_Criador.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Criador:", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
 
         btn_Concluido.setText("Concluído");
         btn_Concluido.addActionListener(new java.awt.event.ActionListener() {
@@ -79,28 +73,18 @@ public class TelaDetalheProjeto extends javax.swing.JFrame {
         lbl_Descricao.setForeground(new java.awt.Color(255, 255, 255));
         lbl_Descricao.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lbl_Descricao.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        lbl_Descricao.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "\"Descrição do projeto\"", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Liberation Sans", 0, 15), new java.awt.Color(255, 255, 255))); // NOI18N
-
-        btn_Procurar.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        btn_Procurar.setText("Procurar");
+        lbl_Descricao.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "\"Descrição do projeto\"", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Segoe UI", 0, 12), new java.awt.Color(255, 255, 255))); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(88, 88, 88)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(225, 225, 225)
-                        .addComponent(txt_ID, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(45, 45, 45)
-                        .addComponent(btn_Procurar, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(88, 88, 88)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(lbl_Data, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(lbl_Descricao, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 624, Short.MAX_VALUE)
-                            .addComponent(lbl_Criador, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                    .addComponent(lbl_Data, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lbl_Descricao, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 624, Short.MAX_VALUE)
+                    .addComponent(lbl_Criador, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(88, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -121,11 +105,7 @@ public class TelaDetalheProjeto extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(15, 15, 15)
                 .addComponent(lbl_Tema, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txt_ID, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btn_Procurar, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addGap(60, 60, 60)
                 .addComponent(lbl_NomeDoProjeto, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(32, 32, 32)
                 .addComponent(lbl_Data)
@@ -144,17 +124,16 @@ public class TelaDetalheProjeto extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 12, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(7, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -202,13 +181,11 @@ public class TelaDetalheProjeto extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_Concluido;
     private javax.swing.JButton btn_Curtir;
-    private javax.swing.JButton btn_Procurar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lbl_Criador;
     private javax.swing.JLabel lbl_Data;
     private javax.swing.JLabel lbl_Descricao;
     private javax.swing.JLabel lbl_NomeDoProjeto;
     private javax.swing.JLabel lbl_Tema;
-    private javax.swing.JTextField txt_ID;
     // End of variables declaration//GEN-END:variables
 }
